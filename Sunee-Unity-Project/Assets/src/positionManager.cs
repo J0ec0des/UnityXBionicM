0c74d9f7737c556379f61b774ada27303bff6fad
Joe <joekojima3@gmail.com>
2023-06-07 11:56:15 +0900

    Altered and implemented interpolated version of position manager to fix bugs and added catch to exception, altered polar coordinate to cartesian position control to account for change in data of phi

--- Sunee-Unity-Project/Assets/src/positionManager.cs
+++ Sunee-Unity-Project/Assets/src/positionManager.cs
@@ -31,14 +31,18 @@ public class positionManager : MonoBehaviour
     
     public GameObject ankl;
     public Readingcsv script;
+
+    private Vector3 kneetarget, ankltarget;
     
     // public List<Hiplist> position = new List<Hiplist>();
     // public List<Kneelist> kneeposition = new List<Kneelist>();
     // public List<Ankllist> anklposition = new List<Ankllist>();
 
     public float bodyspeed = 4;
-    //placeholder for speed variable, will change according to speed determination by prosthetic
+    //placeholder for body speed variable, will change according to speed determination by prosthetic
     // delete value when implementing speed variation(experimental script 2)
+
+    [SerializeField] float interpolationspeed = 1000;
 
     // Start is called before the first frame update
     
@@ -50,28 +54,41 @@ public class positionManager : MonoBehaviour
             //hip.transform.localPosition = new Vector3(data.hip_pos_x * 10, data.hip_pos_z * 10, data.hip_pos_y * 10 ); 
             //deleted due to values being 0.
 
-            knee.transform.localPosition = new Vector3(data.knee_pos_x * 10, data.knee_pos_z * 10, data.knee_pos_y * 10);
-            ankl.transform.localPosition = new Vector3(data.ankl_pos_x * 10, data.ankl_pos_z * 10, data.ankl_pos_y * 10);
+            // knee.transform.localPosition = new Vector3(data.knee_pos_x * 10, data.knee_pos_z * 10, data.knee_pos_y * 10);
+            // ankl.transform.localPosition = new Vector3(data.ankl_pos_x * 10, data.ankl_pos_z * 10, data.ankl_pos_y * 10);
             //Debug.Log(data.time.ToString("F4") + hip.transform.position.ToString("F5"));
             
             //making an interpolated version of the transform function to account for the 30ms latency of prosthetics' BLE:
             //first using current vector3 function types:
-            // kneetarget = new Vector3(data.knee_pos_x * 10, data.knee_pos_z * 10, data.knee_pos_y * 10);
-            // ankltarget = new Vector3(data.ankl_pos_x * 10, data.ankl_pos_z * 10, data.ankl_pos_y * 10);
-            // var step = data.footspeed * Time.deltaTime;
-            //above only possible if footspeed data is a data value given by the prosthetic leg
-            // knee.transform.localPosition = Vector3.MoveTowards(transform.position, kneetarget, step);
-            // ankl.transform.localPosition = Vector3.MoveTowards(transform.position, ankltarget, step);
+            kneetarget = new Vector3(data.knee_pos_x * 10, data.knee_pos_z * 10, data.knee_pos_y * 10);
+            ankltarget = new Vector3(data.ankl_pos_x * 10, data.ankl_pos_z * 10, data.ankl_pos_y * 10);
+            var step = interpolationspeed * Time.deltaTime;
+            //interpolationspeed may become a data value given by the prosthetic leg but mostly unnecessary
+            knee.transform.localPosition = Vector3.MoveTowards(transform.localPosition, kneetarget, step);
+            ankl.transform.localPosition = Vector3.MoveTowards(transform.localPosition, ankltarget, step);
+            
+            // Check if the position of the target and actual are approximately equal.
+            if (Vector3.Distance(knee.transform.localPosition, kneetarget) > 0.01f)
+            {
+                //force move
+                knee.transform.localPosition = new Vector3(data.knee_pos_x * 10, data.knee_pos_z * 10, data.knee_pos_y * 10);
+            }
+            if (Vector3.Distance(ankl.transform.localPosition, ankltarget) > 0.01f)
+            {
+                //force move
+                ankl.transform.localPosition = new Vector3(data.ankl_pos_x * 10, data.ankl_pos_z * 10, data.ankl_pos_y * 10);
+            }
+            
             
 
 
             //Experimental script1(angle to Vector3 positions)
-            //float knee_x = data.thigh_length * sin(data.hip_angl);
-            //float knee_z = data.thigh_length * cos(data.hip_angl);
-            //float knee_y = data.thigh_length * sin(data.hip_phi);
-            //float ankl_x = data.shin_length * sin(data.knee_angl);
-            //float ankl_z = data.shin_length * cos(data.knee_angl);
-            //float ankl_y = data.shin_length * sin(data.knee_phi);
+            // float knee_x = data.thigh_length * cos(data.hip_angl) * sin(data.hip_phi) / (Math.pow(cos(data.hip_phi)) * Math.Pow(sin(data.hip_angl)) + Math.Pow(sin(data.hip_phi)));
+            // float knee_z = data.thigh_length * cos(data.hip_phi) * sin(data.hip_angl) / (Math.pow(cos(data.hip_phi)) * Math.Pow(sin(data.hip_angl)) + Math.Pow(sin(data.hip_phi)));
+            // float knee_y = data.thigh_length * sin(data.hip_phi) * sin(data.hip_angl) / (Math.pow(cos(data.hip_phi)) * Math.Pow(sin(data.hip_angl)) + Math.Pow(sin(data.hip_phi)));
+            // float ankl_x = data.shin_length * cos(data.knee_angl) * sin(data.knee_phi) / (Math.pow(cos(data.knee_phi)) * Math.Pow(sin(data.knee_angl)) + Math.Pow(sin(data.knee_phi)));
+            // float ankl_z = data.shin_length * cos(data.knee_phi) * sin(data.knee_angl) / (Math.pow(cos(data.knee_phi)) * Math.Pow(sin(data.knee_angl)) + Math.Pow(sin(data.knee_phi)));
+            // float ankl_y = data.shin_length * sin(data.knee_phi) * sin(data.knee_angl) / (Math.pow(cos(data.knee_phi)) * Math.Pow(sin(data.knee_angl)) + Math.Pow(sin(data.knee_phi)));
             //knee.transform.localPosition = new Vector3(knee_x, knee_y, knee_z);
             //ankl.transform.localPosition = new Vector3(knee_x + ankl_x, knee_z + ankl_z, knee_y + ankl_y);
 
